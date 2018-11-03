d26e7c1a5bfd5fb77ed57119b0a0b31dc3e7a290
jungeun0 <sky_fall7@naver.com>
2018-11-03 12:04:18 +0900

    6-4

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "caicMatrix.h"
+#define input 10
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
 	
+	int i;
+	int A[input];
 	
-	int A[ROWS][COLS] = {
-		{2,3,0},
-		{8,9,1},
-		{7,0,5}};
+	//1.변수 선언
+	for(i=0;i<input;i++){
+		scanf("%d", &A[i] ); }
 	
-	int B[ROWS][COLS] = {
-		{1,0,0},
-		{0,1,0},
-		{0,0,1}};
-		
-	int C[ROWS][COLS];
+	//2.입력값 받기
 	
-	addMatrix(A,B,C);
-	printMatrix(C);
-		
 	
-	return 0;
+	
+	//3.sorting
+	
+	for(i=0;i<input;i++){
+		printf("%d ", A[i]); }
+	
+	
+	//4.출력
 }
